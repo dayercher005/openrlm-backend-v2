4e48fd66b3c5d89773ba2017facc08d44a72a585
Dayer <dayerxr.cher@gmail.com>
2026-04-28 16:42:16 +0800

    fix: fiex module import errors and updated routing

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from models.models import Conversation, Message
+from src.models.models import Conversation, Message
 
-from routers.new_conversation import router as new_conversation_router
-from routers.conversation_list import router as conversation_list_router
+from src.routers.new_conversation import router as new_conversation_router
+from src.routers.conversation_list import router as conversation_list_router
+from src.routers.conversation import router as conversation_router
 
 app = FastAPI()
 
@@ -21,7 +22,8 @@ app.add_middleware(
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"status": "OK"}
 
-app.include_router(new_conversation_router, prefix="/")
+app.include_router(new_conversation_router, prefix="/new-conversation")
 app.include_router(conversation_list_router, prefix="/conversations")
+app.include_router(conversation_router, prefix="/conversations/{conversation_id}")
